a40f857bbe13bbe28d450e8eea7f3252440266c8
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-04-20 22:48:08 +0300

    Enemy_test

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+}
