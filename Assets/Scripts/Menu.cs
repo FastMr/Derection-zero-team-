64295264e78dee7e6975db151feac3aa657dbd32
FastMr <RomanovMatvey1@gmail.com>
2024-04-22 15:03:02 +0300

    Партия оценить!

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("PlayerController");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
