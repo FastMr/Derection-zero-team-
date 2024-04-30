ef7ef37f6c4c1fd1a369804e1c53339918709c6e
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-04-30 18:04:56 +0300

    Сырые патроны
    На счет того что они сырые обсудим в ДС

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    public int ammoAmount = 15; // Количество патронов, которое будет добавлено
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        // Проверить, является ли объект, который столкнулся с триггером, игроком
+        if (collision.gameObject.tag == "Player")
+        {
+            // Получить компонент AmmoPluseText игрока
+            AmmoPluseText ammoPluseText = collision.gameObject.GetComponent<AmmoPluseText>();
+
+            // Проверить, есть ли у игрока компонент AmmoPluseText
+            if (ammoPluseText != null)
+            {
+                // Увеличить значение AmmoPluse на количество патронов
+                ammoPluseText.AmmoPluse += ammoAmount;
+            }
+
+            // Уничтожить ящик с патронами
+            Destroy(gameObject);
+        }
+    }
+}
