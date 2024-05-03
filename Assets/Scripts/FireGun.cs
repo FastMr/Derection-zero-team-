61e33c44bdfd54935a2d260f827f02e6f14f68b1
FastMr <RomanovMatvey1@gmail.com>
2024-05-03 21:47:28 +0300

    На 90% готовая система патронов
    авто перезарядка полностью готова а перезарядка по кнопке не совсем

--- Assets/Scripts/FireGun.cs
+++ Assets/Scripts/FireGun.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 
 public class FireGun : MonoBehaviour
 {
-    public float Bullet = 15;
+    public float BulletMax = 15;
+    public float Bullet = 0;
     public float Ammo = 30;
+    public float ReloadTime = 3;
+
+    private float BulletFull;
+    private bool Reloading = false;
 
     public Transform BulletPrefab;
     public Transform Point;
-    public float TimeToSpawn = 0.1f;
-
+    public float RateOfFire = 0.3f;
 
     private float _timeSpawn;
 
@@ -22,7 +26,7 @@ public class FireGun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _timeSpawn = TimeToSpawn;
+        _timeSpawn = RateOfFire;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -30,13 +34,50 @@ public class FireGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (TimeToSpawn <= 0 && Bullet >= 0 && Input.GetMouseButton(0))
+        //стрельба 
+        if (RateOfFire <= 0 && Bullet > 0 && Input.GetMouseButton(0) && Reloading == false)
         {
             Instantiate(BulletPrefab, Point.position, Point.rotation);
             Bullet -= 1;
-            TimeToSpawn = _timeSpawn;
+            RateOfFire = _timeSpawn;
             
         }
-        TimeToSpawn -= Time.deltaTime;
+        RateOfFire -= Time.deltaTime;
+
+        //авто перезарядка
+        if (Bullet <= 0)
+        {
+            if (Reloading == false)
+            {
+               Reload();
+            }
+        }
+
+        //перезарядка по кнопке (недоделано)
+        if (Input.GetKeyDown(KeyCode.R) && Reloading != true)
+        {
+            Reload();
+        }
+    }
+    private void Reload()
+    {
+        Reloading = true;
+        //то сколько нужно зарядить
+        BulletFull = BulletMax - Bullet;
+        if (0 < Ammo)
+        {
+            Invoke("Reloaded", ReloadTime);
+        }
+    }
+    private void Reloaded()
+    {
+        Bullet = BulletMax;
+        Ammo -= BulletFull;
+        Reloading = false;
+        if (Ammo < 0)
+        {
+            Bullet += Ammo;
+            Ammo = 0;
+        }
     }
 }
