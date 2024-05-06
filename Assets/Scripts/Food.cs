cb1f1b10a2438d5d6f03fb9396dd376f0e0cb6b2
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-05-06 15:46:49 +0300

    Еда без инвентаря

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Проверить, нажата ли клавиша E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Получить компонент Hunger игрока
+            Hunger hunger = GetComponent<Hunger>();
+
+            // Проверить, есть ли у игрока компонент Hunger
+            if (hunger != null)
+            {
+                // Увеличить голод игрока на 50
+                hunger.Eat(50);
+            }
+        }
+    }
+}
