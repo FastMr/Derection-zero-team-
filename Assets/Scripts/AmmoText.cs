51094bd4f7401dcdff748ea0961b45626c3d3b71
FastMr <RomanovMatvey1@gmail.com>
2024-05-10 21:16:41 +0300

    123456tygfhjuiy7trfgcbhjuy76tr5dgfhuy76tyghjui7y6tgfhjuiy7ghbjkiou8y7ghjkiou87y6gh

---
+++ Assets/Scripts/AmmoText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmmoText : MonoBehaviour
+{
+    public TextMeshProUGUI ammoText;
+    public TextMeshProUGUI bulletText;
+
+    private float ammo;
+    private float bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = GameObject.Find("Player");
+        FireGun fireGun = go.GetComponent<FireGun>();
+        bullet = fireGun.Bullet;
+        ammo = fireGun.Ammo;
+
+        ammoText.text = ammo.ToString();
+        bulletText.text = bullet.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ammoText.text = ammo.ToString();
+            bulletText.text = bullet.ToString();
+        }
+        
+    }
+}
