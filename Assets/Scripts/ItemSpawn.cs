749626666d694b3eff98d9a7370be6b01c2b8ff0
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-05-12 19:53:50 +0300

    SJ

---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class ItemSpawn : MonoBehaviour
+{
+    public Food ItemPrefab;
+    public float TimeToSpawn = 10;
+
+    private float _timeSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timeSpawn = TimeToSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeToSpawn <= 0)
+        {
+            Instantiate(ItemPrefab, transform.position, transform.rotation);
+            TimeToSpawn = _timeSpawn;
+        }
+        TimeToSpawn -= Time.deltaTime;
+    }
+}
