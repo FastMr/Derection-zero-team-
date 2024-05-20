7fbe95b5ef8d47e1fe3537d1958cc29b9eaf134b
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-05-20 17:27:30 +0300

    бляяяяяят

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,11 +2,13 @@
 
 public class Food : MonoBehaviour
 {
+    public GameObject Eshka;
     private void OnTriggerStay(Collider other)
     {
         // Check if the object that collided with the food is the player
         if (other.gameObject.CompareTag("Player"))
         {
+            Eshka.SetActive(true);
             // Check if the player is holding down the "E" key
             if (Input.GetKey(KeyCode.E))
             {
@@ -18,8 +20,19 @@ public class Food : MonoBehaviour
                 {
                     hunger.Eat(50);
                     Destroy(gameObject);
+                    Eshka.SetActive(false);
                 }
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the object that exited the trigger is the player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Deactivate the Eshka object
+            Eshka.SetActive(false);
         }
     }
 }
